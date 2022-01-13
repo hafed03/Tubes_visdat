8f6b1a7cfa4a524f655a739db6b34faab6b185ff
hafed03 <60875000+hafed03@users.noreply.github.com>
2022-01-13 19:03:47 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -162,7 +162,7 @@ new_case.yaxis.formatter = NumeralTickFormatter(format="00")
 # Definisikan line / proses render line
 total_case.line('Date', 'TotalCases',
               color='green', legend_label='Total Kasus Pulau Sumatera',
-              source=sumatera_cds,line_alpha=0.6)
+              source=sumatera_cds,line_bold=2)
 total_case.line('Date', 'TotalCases',
               color='blue', legend_label='Total Kasus Pulau Jawa',
               source=jawa_cds)
